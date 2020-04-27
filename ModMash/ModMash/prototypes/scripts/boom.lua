1b64d8bc4fb0836c2a4c662e3340a515aed4cdee
Pixel Whipped <39611679+pixelwhipped@users.noreply.github.com>
2020-04-27 15:46:28 +1200

    mine updates

--- ModMash/ModMash/prototypes/scripts/boom.lua
+++ ModMash/ModMash/prototypes/scripts/boom.lua
@@ -2,24 +2,17 @@
 
 local is_valid  = modmash.util.is_valid
 
-local landmines = nil
-
 local local_init = function()	
 	if global.modmash.landmines == nil then global.modmash.landmines = {} end	
-	landmines = global.modmash.landmines
+	if global.modmash.landmines.highlights == nil then global.modmash.landmines.highlights = {} end
 	end
 
 local local_load = function()	
-	if global.modmash.landmines ~= nil then
-		landmines = global.modmash.landmines
-	end
+	if global.modmash.landmines ~= nil then landmines = global.modmash.landmines end
 	end
 
 local local_on_configuration_changed = function(f)
-	if f.mod_changes["modmash"].old_version < "0.18.39" then	
-		global.modmash.landmines = {}	
-		landmines = global.modmash.landmines
-	end
+	local_init()
 	end
 
 local local_create_element = function(parent,type,name,caption,tooltip)
@@ -44,7 +37,7 @@ local local_update_gui = function()
 	local inner_update = function(pi)
 		local button_flow =  mod_gui.get_button_flow(game.players[pi])
 		
-		if #landmines > 0 then
+		if #global.modmash.landmines > 0 then
 			for k = #button_flow.children, 1, -1 do
 				local button = button_flow.children[k]
 				if button and button.name == "landmine-toggle-button" then
@@ -71,17 +64,18 @@ local local_update_gui = function()
 	
 
 local local_landmine_added = function(entity)
+	
 	if entity.type == "land-mine"  then		
-		table.insert(landmines, entity)		
+		table.insert(global.modmash.landmines, entity)		
 		local_update_gui()
 	end	
 end
 
 local local_landmine_removed = function(entity)
 	if entity.type == "land-mine"  then				
-		for index, landmine in pairs(landmines) do
+		for k=0, #global.modmash.landmines do local landmine = global.modmash.landmines[k]
 			if  landmine == entity then
-				table.remove(landmines, index)		
+				table.remove(global.modmash.landmines, index)		
 				local_update_gui()
 				return
 			end
@@ -103,10 +97,10 @@ end
 function landmine_on_gui_click(event)
 	if event.element.name == "landmine-toggle-button" then
 		local copy = {}
-		for k=1 , #landmines do
-			table.insert(copy,landmines[k])
+		for k=1 , #global.modmash.landmines do
+			table.insert(copy,global.modmash.landmines[k])
 		end
-		landmines = {}
+		global.modmash.landmines = {}
 		local_update_gui()
 
 		for k = 1, #copy do local landmine = copy[k]
@@ -121,12 +115,38 @@ function landmine_on_gui_click(event)
 	end
 	end
 
+local local_on_entity_selected = function(player,entity)
+	local player_index = player.index
+	if global.modmash.landmines.highlights == nil then global.modmash.landmines.highlights = {} end
+	if is_valid(entity) and entity.name == "nuclear-land-mine" then		
+		local id = rendering.draw_circle
+		{
+			surface = entity.surface,
+			players = {player},
+			filled = true,
+			color = {r = 1, g = 0.1, b = 0, a = 0.1},
+			draw_on_ground = true,
+			width = 36,
+			filled = false,
+			target = entity,
+			only_in_alt_mode = false,
+			radius = 36
+			--left_top = area[1],
+			--right_bottom = area[2]
+		}
+		global.modmash.landmines.highlights[player_index] = id
+	elseif global.modmash.landmines.highlights[player_index] ~= nil then
+		rendering.destroy(global.modmash.landmines.highlights[player_index])
+	end
+end
+
 local control = {
 	on_init = local_init,
 	on_load = local_load,
 	on_added = local_landmine_added,
 	on_removed = local_landmine_removed,
 	on_configuration_changed = local_on_configuration_changed,
+	on_selected = local_on_entity_selected
 }
 
 modmash.register_script(control)
