f86d6a66212374292126c55e1c1a2110f15e1b63
Kingdud <kingdud@quietly.win>
2020-09-30 18:34:02 +0100

    Changes to bring this up to date with the current release (IE 1.26)

--- ModMash/HeroTurrets/settings.lua
+++ ModMash/HeroTurrets/settings.lua
@@ -16,5 +16,26 @@
 		minimum_value = 0,
 		maximum_value = 100,
 		order = "b"
-	}
+	},{
+		type = "bool-setting",
+		name = "heroturrets-allow-ghost-rank",
+		setting_type = "runtime-global",
+		default_value = false,
+		order = "c"
+	},
+	--[[{
+		type = "bool-setting",
+		name = "heroturrets-allow-blueprint-rank",
+		setting_type = "runtime-global",
+		default_value = false,
+		order = "d"
+	},]]
+	{
+		type = "string-setting",
+		name = "heroturrets-kill-counter",
+		setting_type = "startup",
+		default_value = "Fuzzy",
+		allowed_values = {"Fuzzy", "Exact"},
+		order = "e"
+	},
 })
